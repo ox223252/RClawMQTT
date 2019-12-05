a4ac22caba3437caef6038ac1d0f915e5b0c7c99
ox223252 <23252@laposte.net>
2019-12-05 20:55:41 +0100

    Simplify source code
    Modifications qui seront validées :
    	modifié :         src/parseCmdMQTT.c

--- src/parseCmdMQTT.c
+++ src/parseCmdMQTT.c
@@ -5,306 +5,87 @@
 
 #include <stdint.h>
 
-// /
-// └── Roboclaw
-//     └── R2O / O2R
-//         └── set / get
-//             ├── Codeur
-//             │   ├── Left
-//             │   │   └── Value
-//             │   └── Right
-//             │       └── Value
-//             └── Motor
-//                 ├── Left
-//                 │   ├── Acc
-//                 │   ├── D
-//                 │   ├── I
-//                 │   ├── P
-//                 │   ├── QPPS
-//                 │   └── Speed
-//                 │       ├── order
-//                 │       └── value
-//                 └── Right
-//                     ├── Acc
-//                     ├── D
-//                     ├── I
-//                     ├── P
-//                     ├── QPPS
-//                     └── Speed
-//                         ├── order
-//                         └── value
+static const char *_parseCmdMQTT_available[] = {
+	"roboclaw/r2o/set/codeur/m1/value",
+	"roboclaw/r2o/set/codeur/m2/value",
+	"roboclaw/r2o/set/motor/m1/acc",
+	"roboclaw/r2o/set/motor/m1/d",
+	"roboclaw/r2o/set/motor/m1/i",
+	"roboclaw/r2o/set/motor/m1/p",
+	"roboclaw/r2o/set/motor/m1/qpps",
+	"roboclaw/r2o/set/motor/m1/speed/order",
+	"roboclaw/r2o/set/motor/m1/speed/value",
+	"roboclaw/r2o/set/motor/m2/acc",
+	"roboclaw/r2o/set/motor/m2/d",
+	"roboclaw/r2o/set/motor/m2/i",
+	"roboclaw/r2o/set/motor/m2/p",
+	"roboclaw/r2o/set/motor/m2/qpps",
+	"roboclaw/r2o/set/motor/m2/speed/order",
+	"roboclaw/r2o/set/motor/m2/speed/value",
+	"roboclaw/o2r/set/codeur/m1/value",
+	"roboclaw/o2r/set/codeur/m2/value",
+	"roboclaw/o2r/set/motor/m1/acc",
+	"roboclaw/o2r/set/motor/m1/d",
+	"roboclaw/o2r/set/motor/m1/i",
+	"roboclaw/o2r/set/motor/m1/p",
+	"roboclaw/o2r/set/motor/m1/qpps",
+	"roboclaw/o2r/set/motor/m1/speed/order",
+	"roboclaw/o2r/set/motor/m1/speed/value",
+	"roboclaw/o2r/set/motor/m2/acc",
+	"roboclaw/o2r/set/motor/m2/d",
+	"roboclaw/o2r/set/motor/m2/i",
+	"roboclaw/o2r/set/motor/m2/p",
+	"roboclaw/o2r/set/motor/m2/qpps",
+	"roboclaw/o2r/set/motor/m2/speed/order",
+	"roboclaw/o2r/set/motor/m2/speed/value",
+	"roboclaw/r2o/get/codeur/m1/value",
+	"roboclaw/r2o/get/codeur/m2/value",
+	"roboclaw/r2o/get/motor/m1/acc",
+	"roboclaw/r2o/get/motor/m1/d",
+	"roboclaw/r2o/get/motor/m1/i",
+	"roboclaw/r2o/get/motor/m1/p",
+	"roboclaw/r2o/get/motor/m1/qpps",
+	"roboclaw/r2o/get/motor/m1/speed/order",
+	"roboclaw/r2o/get/motor/m1/speed/value",
+	"roboclaw/r2o/get/motor/m2/acc",
+	"roboclaw/r2o/get/motor/m2/d",
+	"roboclaw/r2o/get/motor/m2/i",
+	"roboclaw/r2o/get/motor/m2/p",
+	"roboclaw/r2o/get/motor/m2/qpps",
+	"roboclaw/r2o/get/motor/m2/speed/order",
+	"roboclaw/r2o/get/motor/m2/speed/value",
+	"roboclaw/o2r/get/codeur/m1/value",
+	"roboclaw/o2r/get/codeur/m2/value",
+	"roboclaw/o2r/get/motor/m1/acc",
+	"roboclaw/o2r/get/motor/m1/d",
+	"roboclaw/o2r/get/motor/m1/i",
+	"roboclaw/o2r/get/motor/m1/p",
+	"roboclaw/o2r/get/motor/m1/qpps",
+	"roboclaw/o2r/get/motor/m1/speed/order",
+	"roboclaw/o2r/get/motor/m1/speed/value",
+	"roboclaw/o2r/get/motor/m2/acc",
+	"roboclaw/o2r/get/motor/m2/d",
+	"roboclaw/o2r/get/motor/m2/i",
+	"roboclaw/o2r/get/motor/m2/p",
+	"roboclaw/o2r/get/motor/m2/qpps",
+	"roboclaw/o2r/get/motor/m2/speed/order",
+	"roboclaw/o2r/get/motor/m2/speed/value",
+	"roboclaw/o2r/verify/cmd/length",
+	NULL
+};
 
 uint32_t getIdFrom ( const char * str )
 {
 	uint32_t rt = 0;
-	if ( rt++, !strcmp( str, "/roboclaw/r2o/set/codeur/m1/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/codeur/m2/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m1" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m1/acc" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m1/d" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m1/i" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m1/p" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m1/qpps" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m1/speed/order" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m1/speed/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m2" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m2/acc" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m2/d" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m2/i" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m2/p" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m2/qpps" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m2/speed/order" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/set/motor/m2/speed/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/codeur/m1/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/codeur/m1/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m1/acc" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m1/d" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m1/i" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m1/p" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m1/qpps" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m1/speed/order" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m1/speed/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m2/acc" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m2/d" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m2/i" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m2/p" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m2/qpps" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m2/speed/order" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/set/motor/m2/speed/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/codeur/m1/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/codeur/m2/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m1/acc" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m1/d" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m1/i" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m1/p" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m1/qpps" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m1/speed/order" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m1/speed/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m2/acc" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m2/d" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m2/i" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m2/p" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m2/qpps" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m2/speed/order" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/r2o/get/motor/m2/speed/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/codeur/m1/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/codeur/m2/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m1/acc" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m1/d" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m1/i" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m1/p" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m1/qpps" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m1/speed/order" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m1/speed/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m2/acc" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m2/d" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m2/i" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m2/p" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m2/qpps" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m2/speed/order" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/get/motor/m2/speed/value" ) )
-	{
-		return ( rt );
-	}
-	else if ( rt++, !strcmp( str, "/roboclaw/o2r/verify/cmd/length" ) )
-	{
-		return ( rt );
-	}
+	while ( !_parseCmdMQTT_available[ rt ] )
+	{
+		if ( !strcmp( str, _parseCmdMQTT_available[ rt ] ) )
+		{
+			return ( rt + 1 );
+		}
+		rt++;
+	}
+
 	return ( 0 );
 }
 
